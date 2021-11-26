3952f31936eba91d6a589f8672ba9f0004a36b00
Dufresne <philippe.dufresne35@gmail.com>
2021-11-26 12:17:44 +0100

    test generic

--- R/RenDate.R
+++ R/RenDate.R
@@ -10,7 +10,7 @@
 #  http://www.r-project.org/Licenses/
 #  _________________________________________________________________________________
 
-# Version 2021-11-19
+# Version 2021-11-26
 #
 
 #' RenDate; inspiré du package Bchron (https://cran.r-project.org/web/packages/Bchron/index.html) développé par Andrew Parnell <Andrew.Parnell at mu.ie>, 
@@ -173,13 +173,13 @@ date_gaussian <- function( mean = 0, sd = 10, time.grid.min = -1000, time.grid.m
   class(out) = "RenDate"
   return(out)
 }
+# HPD ----
 #' Calcul le hpd (hdr) sur une densité de probabilité de date
 #' @param date densité produite par la fonction calibrate, générant un objet de class "RenDate" 
 #' @param prob requested surface value [0, 1]
 #' @export
 setGeneric("hpd", package = "RenDate", valueClass = "list",
- function(date, prob = 0.95) 
-{
+ function(date, prob = 0.95) {
   
   # A function to return the HPD interval for a date object which should have an timeGrid and a densities argument
   # I was previously using the hdrcde package but this has several unnecessary dependencies
@@ -224,85 +224,47 @@ setGeneric("hpd", package = "RenDate", valueClass = "list",
 }
 )
 
+# PLOT ----
 #' Trace des courbe de densité
 #' @param withHDR Calcul le hdr (hpd) et remplie la surface correspondant
 #' @param dateHeigth Fixe la hauteur des densités, quand withPositions est TRUE
 #' @param normalize force le maximum à la valeur 1
 #' @export
-plot.RenDate <- function(x, withPositions = FALSE, pause = FALSE, dateHeight = 30, normalize= FALSE, borderCol = NULL,
+setGeneric("plot", package = "RenDate",
+  function(x, withPositions = FALSE, pause = FALSE, dateHeight = 30, normalize= FALSE, borderCol = NULL,
                          fillCols = rep('gray', length(x)), withHDR = TRUE, hdrCol = 'darkgray',
-                         ...) 
-{
+                         ...) {
   
-  # Get extra arguments if provided
-  ex = list(...)
-  
-  if(is.null(ex$xlab)) ex$xlab = 'Date'
-  if(is.null(ex$ylab)) ex$ylab = ifelse(withPositions,'Position','Density')
-  
-  # First plot for individual dates
-  if(length(x)==1) {
-    if(normalize == TRUE) 
-    {
-      fac <- max(x[[1]]$densities)
-    } else {
-      fac <- x[[1]]$timeScale
-    }
-    ag = x[[1]]$timeGrid
-    den = x[[1]]$densities / fac
-    ex$x = ag
-    ex$y = den
-    ex$type = 'l'
-    ex$new = new
-    if(is.null(ex$main)) ex$main = names(x)
-    args = utils::modifyList(ex, list(...))
-    do.call("plot", args)
-    #graphics::mtext(paste(x[[1]]$calCurves),side=1,line=4,adj=0,cex=0.6)
-    if(withHDR) {
-      my_hdr = hpd(x[[1]])
-      for(j in 1:length(my_hdr)) {
-        #x_seq = seq(my_hdr[[j]][1], my_hdr[[j]][2], by = 1)
-        #y_lookup = match(x_seq, ag)
-        #y_seq = den[y_lookup]
-        y_lookup <- match(my_hdr[[j]], ag)
-        x_seq <- ag[y_lookup[1]: y_lookup[2]]
-        y_seq = den[y_lookup[1]: y_lookup[2]]
-        graphics::polygon(c(my_hdr[[j]][1], x_seq, my_hdr[[j]][2]),
-                          c(0, y_seq, 0),
-                          col = hdrCol,
-                          border = NA)
-      }
-    }
+    # Get extra arguments if provided
+    ex = list(...)
     
-  }
-  
-  # Now for multiple dates without depths
-  if(length(x)>1 & withPositions==FALSE) {
-    for(i in 1:length(x)) {
+    if(is.null(ex$xlab)) ex$xlab = 'Date'
+    if(is.null(ex$ylab)) ex$ylab = ifelse(withPositions,'Position','Density')
+    
+    # First plot for individual dates
+    if(length(x)==1) {
       if(normalize == TRUE) 
       {
-        fac <- max(x[[i]]$densities)
+        fac <- max(x[[1]]$densities)
       } else {
-        fac <- x[[i]]$timeScale
+        fac <- x[[1]]$timeScale
       }
-      ex_curr = ex
-      ag = x[[i]]$timeGrid
-      den = x[[i]]$densities / fac
-      ex_curr$x = ag
-      ex_curr$y = den
-      ex_curr$type = 'l'
-      ex_curr$new = new
-      if(is.null(ex_curr$main)) ex_curr$main = names(x)[i]
-      args = utils::modifyList(ex_curr, list(...))
+      ag = x[[1]]$timeGrid
+      den = x[[1]]$densities / fac
+      ex$x = ag
+      ex$y = den
+      ex$type = 'l'
+      ex$new = new
+      if(is.null(ex$main)) ex$main = names(x)
+      args = utils::modifyList(ex, list(...))
       do.call("plot", args)
-      # graphics::mtext(paste(x[[i]]$calCurves),side=1,line=4,adj=0,cex=0.6)
+      #graphics::mtext(paste(x[[1]]$calCurves),side=1,line=4,adj=0,cex=0.6)
       if(withHDR) {
-        my_hdr = hpd(x[[i]])
+        my_hdr = hpd(x[[1]])
         for(j in 1:length(my_hdr)) {
           #x_seq = seq(my_hdr[[j]][1], my_hdr[[j]][2], by = 1)
           #y_lookup = match(x_seq, ag)
           #y_seq = den[y_lookup]
-          
           y_lookup <- match(my_hdr[[j]], ag)
           x_seq <- ag[y_lookup[1]: y_lookup[2]]
           y_seq = den[y_lookup[1]: y_lookup[2]]
@@ -312,111 +274,114 @@ plot.RenDate <- function(x, withPositions = FALSE, pause = FALSE, dateHeight = 3
                             border = NA)
         }
       }
-      if(pause) if(i<length(x)) readline('Hit Enter for next plot...')
+      
     }
-  }
-  
-  # Finally for multiple dates with depths
-  if(length(x)>1 & withPositions==TRUE) {
-    xlimits = NULL
-    ylimits = NULL
-    for(i in 1:length(x)) {
-      xlimits = range(c(xlimits,x[[i]]$timeGrid))
-      ylimits = range(c(ylimits,x[[i]]$positions))
-    }
-    #dateHeight=0.2*diff(pretty(ylimits))[1]
-    ylimits[1] = ylimits[1]-dateHeight
-    if(is.null(ex$xlim)) ex$xlim = rev(xlimits)
-    if(is.null(ex$ylim)) ex$ylim = rev(ylimits)
-    ex$x = ex$y = 1
-    ex$type = 'n'
-    ex$new = new
-    if(is.null(ex$main)) ex$main = 'Calibrated dates by position'
-    args = utils::modifyList(ex, list(...))
-    do.call("plot", args)
     
-    for(i in 1:length(x)) {
-      curr_xlimit = range(x[[i]]$timeGrid)
-      curr_pos = x[[i]]$positions
-      graphics::polygon(c(curr_xlimit[1], x[[i]]$timeGrid, curr_xlimit[2]),
-                        c(curr_pos, x[[i]]$positions-x[[i]]$densities*dateHeight/max(x[[i]]$densities), curr_pos),
-                        border=ifelse(is.null(borderCol), NA, borderCol),
-                        col=fillCols[i])
+    # Now for multiple dates without depths
+    if(length(x)>1 & withPositions==FALSE) {
+      for(i in 1:length(x)) {
+        if(normalize == TRUE) 
+        {
+          fac <- max(x[[i]]$densities)
+        } else {
+          fac <- x[[i]]$timeScale
+        }
+        ex_curr = ex
+        ag = x[[i]]$timeGrid
+        den = x[[i]]$densities / fac
+        ex_curr$x = ag
+        ex_curr$y = den
+        ex_curr$type = 'l'
+        ex_curr$new = new
+        if(is.null(ex_curr$main)) ex_curr$main = names(x)[i]
+        args = utils::modifyList(ex_curr, list(...))
+        do.call("plot", args)
+        # graphics::mtext(paste(x[[i]]$calCurves),side=1,line=4,adj=0,cex=0.6)
+        if(withHDR) {
+          my_hdr = hpd(x[[i]])
+          for(j in 1:length(my_hdr)) {
+            #x_seq = seq(my_hdr[[j]][1], my_hdr[[j]][2], by = 1)
+            #y_lookup = match(x_seq, ag)
+            #y_seq = den[y_lookup]
+            
+            y_lookup <- match(my_hdr[[j]], ag)
+            x_seq <- ag[y_lookup[1]: y_lookup[2]]
+            y_seq = den[y_lookup[1]: y_lookup[2]]
+            graphics::polygon(c(my_hdr[[j]][1], x_seq, my_hdr[[j]][2]),
+                              c(0, y_seq, 0),
+                              col = hdrCol,
+                              border = NA)
+          }
+        }
+        if(pause) if(i<length(x)) readline('Hit Enter for next plot...')
+      }
     }
+    
+    # Finally for multiple dates with depths
+    if(length(x)>1 & withPositions==TRUE) {
+      xlimits = NULL
+      ylimits = NULL
+      for(i in 1:length(x)) {
+        xlimits = range(c(xlimits,x[[i]]$timeGrid))
+        ylimits = range(c(ylimits,x[[i]]$positions))
+      }
+      #dateHeight=0.2*diff(pretty(ylimits))[1]
+      ylimits[1] = ylimits[1]-dateHeight
+      if(is.null(ex$xlim)) ex$xlim = rev(xlimits)
+      if(is.null(ex$ylim)) ex$ylim = rev(ylimits)
+      ex$x = ex$y = 1
+      ex$type = 'n'
+      ex$new = new
+      if(is.null(ex$main)) ex$main = 'Calibrated dates by position'
+      args = utils::modifyList(ex, list(...))
+      do.call("plot", args)
+      
+      for(i in 1:length(x)) {
+        curr_xlimit = range(x[[i]]$timeGrid)
+        curr_pos = x[[i]]$positions
+        graphics::polygon(c(curr_xlimit[1], x[[i]]$timeGrid, curr_xlimit[2]),
+                          c(curr_pos, x[[i]]$positions-x[[i]]$densities*dateHeight/max(x[[i]]$densities), curr_pos),
+                          border=ifelse(is.null(borderCol), NA, borderCol),
+                          col=fillCols[i])
+      }
+    }
+    
   }
-  
-}
+)
 
 #' Trace des courbes de densité avec leurs enveloppes d erreur
 #' @param withHDR Calcul le hdr (hdp) et remplie la surface correspondant
 #' @export
-lines.RenDate <- function(x, withPositions=FALSE, pause=FALSE, dateHeight = 30, normalize = FALSE, borderCol = NULL,
+setGeneric("lines", package = "RenDate",
+           function(x, withPositions=FALSE, pause=FALSE, dateHeight = 30, normalize = FALSE, borderCol = NULL,
                           fillCols = rep('gray', length(x)), withHDR = TRUE, hdrCol = 'darkgray', 
-                          ...) 
-{
+                          ...) {
   
-  # Get extra arguments if provided
-  ex = list(...)#as.list(substitute(list(...)))[-1L]
-  
-  if(is.null(ex$xlab)) ex$xlab = 'Date'
-  if(is.null(ex$ylab)) ex$ylab = ifelse(withPositions,'Position','Density')
-  
-  # First plot for individual dates
-  if(length(x)==1) {
-    if(normalize == TRUE) 
-    {
-      fac <- max(x[[1]]$densities)
-    } else {
-      fac <- x[[1]]$timeScale
-    }
-    ag = x[[1]]$timeGrid
-    den = x[[1]]$densities / fac
-    ex$x = ag
-    ex$y = den
-    ex$type = 'l'
-    if(is.null(ex$main)) ex$main = names(x)
-    args = utils::modifyList(ex, list(...))
-    do.call("lines", args)
-    #graphics::mtext(paste(x[[1]]$calCurves),side=1,line=4,adj=0,cex=0.6)
-    if(withHDR) {
-      my_hdr = hpd(x[[1]])
-      for(j in 1:length(my_hdr)) {
-        #x_seq = seq(my_hdr[[j]][1], my_hdr[[j]][2], by = 1)
-        #y_lookup = match(x_seq, ag)
-        #y_seq = den[y_lookup]
-        y_lookup <- match(my_hdr[[j]], ag)
-        x_seq <- ag[y_lookup[1]: y_lookup[2]]
-        y_seq = den[y_lookup[1]: y_lookup[2]]
-        graphics::polygon(c(my_hdr[[j]][1], x_seq, my_hdr[[j]][2]),
-                          c(0, y_seq, 0),
-                          col = hdrCol,
-                          border = NA)
-      }
-    }
+    # Get extra arguments if provided
+    ex = list(...)#as.list(substitute(list(...)))[-1L]
     
-  }
-  
-  # Now for multiple dates without depths
-  if(length(x)>1 & withPositions==FALSE) {
-    for(i in 1:length(x)) {
-      ex_curr = ex
+    if(is.null(ex$xlab)) ex$xlab = 'Date'
+    if(is.null(ex$ylab)) ex$ylab = ifelse(withPositions,'Position','Density')
+    
+    # First plot for individual dates
+    if(length(x)==1) {
       if(normalize == TRUE) 
       {
-        fac <- max(x[[i]]$densities)
+        fac <- max(x[[1]]$densities)
       } else {
-        fac <- x[[i]]$timeScale
+        fac <- x[[1]]$timeScale
       }
-      ag = x[[i]]$timeGrid
-      den = x[[i]]$densities / fac
-      ex_curr$x = ag
-      ex_curr$y = den
-      ex_curr$type = 'l'
-      if(is.null(ex_curr$main)) ex_curr$main = names(x)[i]
-      args = utils::modifyList(ex_curr, list(...))
+      ag = x[[1]]$timeGrid
+      den = x[[1]]$densities / fac
+      ex$x = ag
+      ex$y = den
+      ex$type = 'l'
+      if(is.null(ex$main)) ex$main = names(x)
+      args = utils::modifyList(ex, list(...))
       do.call("lines", args)
-      # graphics::mtext(paste(x[[i]]$calCurves),side=1,line=4,adj=0,cex=0.6)
+      #graphics::mtext(paste(x[[1]]$calCurves),side=1,line=4,adj=0,cex=0.6)
       if(withHDR) {
-        my_hdr = hpd(x[[i]])
+        my_hdr = hpd(x[[1]])
         for(j in 1:length(my_hdr)) {
           #x_seq = seq(my_hdr[[j]][1], my_hdr[[j]][2], by = 1)
           #y_lookup = match(x_seq, ag)
@@ -430,39 +395,77 @@ lines.RenDate <- function(x, withPositions=FALSE, pause=FALSE, dateHeight = 30, 
                             border = NA)
         }
       }
-      if(pause) if(i<length(x)) readline('Hit Enter for next plot...')
+      
     }
-  }
-  
-  # Finally for multiple dates with depths
-  if(length(x)>1 & withPositions==TRUE) {
-    xlimits = NULL
-    ylimits = NULL
-    for(i in 1:length(x)) {
-      xlimits = range(c(xlimits,x[[i]]$timeGrid))
-      ylimits = range(c(ylimits,x[[i]]$positions))
-    }
-    #dateHeight=0.2*diff(pretty(ylimits))[1]
-    ylimits[1] = ylimits[1]-dateHeight
-    if(is.null(ex$xlim)) ex$xlim = rev(xlimits)
-    if(is.null(ex$ylim)) ex$ylim = rev(ylimits)
-    ex$x = ex$y = 1
-    ex$type = 'n'
-    if(is.null(ex$main)) ex$main = 'Calibrated dates by position'
-    args = utils::modifyList(ex, list(...))
-    do.call("lines", args)
     
-    for(i in 1:length(x)) {
-      curr_xlimit = range(x[[i]]$ageGrid)
-      curr_pos = x[[i]]$positions
-      graphics::polygon(c(curr_xlimit[1], x[[i]]$timeGrid, curr_xlimit[2]),
-                        c(curr_pos, x[[i]]$positions-x[[i]]$densities*dateHeight/max(x[[i]]$densities), curr_pos),
-                        border=ifelse(is.null(borderCol),NA,borderCol),
-                        col=fillCols[i])
+    # Now for multiple dates without depths
+    if(length(x)>1 & withPositions==FALSE) {
+      for(i in 1:length(x)) {
+        ex_curr = ex
+        if(normalize == TRUE) 
+        {
+          fac <- max(x[[i]]$densities)
+        } else {
+          fac <- x[[i]]$timeScale
+        }
+        ag = x[[i]]$timeGrid
+        den = x[[i]]$densities / fac
+        ex_curr$x = ag
+        ex_curr$y = den
+        ex_curr$type = 'l'
+        if(is.null(ex_curr$main)) ex_curr$main = names(x)[i]
+        args = utils::modifyList(ex_curr, list(...))
+        do.call("lines", args)
+        # graphics::mtext(paste(x[[i]]$calCurves),side=1,line=4,adj=0,cex=0.6)
+        if(withHDR) {
+          my_hdr = hpd(x[[i]])
+          for(j in 1:length(my_hdr)) {
+            #x_seq = seq(my_hdr[[j]][1], my_hdr[[j]][2], by = 1)
+            #y_lookup = match(x_seq, ag)
+            #y_seq = den[y_lookup]
+            y_lookup <- match(my_hdr[[j]], ag)
+            x_seq <- ag[y_lookup[1]: y_lookup[2]]
+            y_seq = den[y_lookup[1]: y_lookup[2]]
+            graphics::polygon(c(my_hdr[[j]][1], x_seq, my_hdr[[j]][2]),
+                              c(0, y_seq, 0),
+                              col = hdrCol,
+                              border = NA)
+          }
+        }
+        if(pause) if(i<length(x)) readline('Hit Enter for next plot...')
+      }
     }
+    
+    # Finally for multiple dates with depths
+    if(length(x)>1 & withPositions==TRUE) {
+      xlimits = NULL
+      ylimits = NULL
+      for(i in 1:length(x)) {
+        xlimits = range(c(xlimits,x[[i]]$timeGrid))
+        ylimits = range(c(ylimits,x[[i]]$positions))
+      }
+      #dateHeight=0.2*diff(pretty(ylimits))[1]
+      ylimits[1] = ylimits[1]-dateHeight
+      if(is.null(ex$xlim)) ex$xlim = rev(xlimits)
+      if(is.null(ex$ylim)) ex$ylim = rev(ylimits)
+      ex$x = ex$y = 1
+      ex$type = 'n'
+      if(is.null(ex$main)) ex$main = 'Calibrated dates by position'
+      args = utils::modifyList(ex, list(...))
+      do.call("lines", args)
+      
+      for(i in 1:length(x)) {
+        curr_xlimit = range(x[[i]]$ageGrid)
+        curr_pos = x[[i]]$positions
+        graphics::polygon(c(curr_xlimit[1], x[[i]]$timeGrid, curr_xlimit[2]),
+                          c(curr_pos, x[[i]]$positions-x[[i]]$densities*dateHeight/max(x[[i]]$densities), curr_pos),
+                          border=ifelse(is.null(borderCol),NA,borderCol),
+                          col=fillCols[i])
+      }
+    }
+    
   }
-  
-}
+)
 
 #' Trace une courbe avec son enveloppe d erreur à 1 sigma et deux sigma
 #' @export
@@ -505,37 +508,39 @@ mesure_enveloppe <- function(t, mesure, std, col.env = "gray",  col.mesure = "da
   
   
 }
+#  Produit ----
 #' Calcul la combinaison au sens produit de deux densités de class "RenDate"
 #' @param timeScale permet de modifier la grille de temps
 #' @export
-produit.RenDate <- function(date1, date2, timeScale = 1)
-{
-  cal_range_i = range(date1[[1]]$timeGrid)
-  cal_range_d = range(date2[[1]]$timeGrid)
-  cal_range = c(max(cal_range_d[1], cal_range_i[1]), min(cal_range_d[2], cal_range_i[2]))
-  
-  
-  currentTimeGridOut = seq(cal_range[1], cal_range[2], by = timeScale)
-  Inc1 = stats::approx(date1[[1]]$timeGrid, date1[[1]]$densities, xout = currentTimeGridOut, rule = 2)$y
-  Dec1 = stats::approx(date2[[1]]$timeGrid, date2[[1]]$densities, xout = currentTimeGridOut, rule = 2)$y
-  if (sum(Inc1)/timeScale != 1) 
-    paste("surface de date1 !=1", sum(Inc1)/timeScale , sep=" ")
-  
-  if (sum(Dec1)/timeScale != 1) 
-    paste("surface de date2 !=1", sum(Dec1)/timeScale, sep =" " )
-  
-  densitiesOut = NULL
-  ageGridOut = NULL
-  out = list()
-  
-  densitiesOut <- Dec1 * Inc1
-  
-  densitiesOut <- densitiesOut/sum(densitiesOut)
-  out[[1]] = list(  timeGrid = currentTimeGridOut, densities = densitiesOut, timeScale = timeScale)
-  names(out) <- "Combinaison"
-  class(out) <- "RenDate"
-  return(out)
-}
+setGeneric("produit", package = "RenDate",
+  function(date1, date2, timeScale = 1) {
+      cal_range_i = range(date1[[1]]$timeGrid)
+      cal_range_d = range(date2[[1]]$timeGrid)
+      cal_range = c(max(cal_range_d[1], cal_range_i[1]), min(cal_range_d[2], cal_range_i[2]))
+      
+      
+      currentTimeGridOut = seq(cal_range[1], cal_range[2], by = timeScale)
+      Inc1 = stats::approx(date1[[1]]$timeGrid, date1[[1]]$densities, xout = currentTimeGridOut, rule = 2)$y
+      Dec1 = stats::approx(date2[[1]]$timeGrid, date2[[1]]$densities, xout = currentTimeGridOut, rule = 2)$y
+      if (sum(Inc1)/timeScale != 1) 
+        paste("surface de date1 !=1", sum(Inc1)/timeScale , sep=" ")
+      
+      if (sum(Dec1)/timeScale != 1) 
+        paste("surface de date2 !=1", sum(Dec1)/timeScale, sep =" " )
+      
+      densitiesOut = NULL
+      ageGridOut = NULL
+      out = list()
+      
+      densitiesOut <- Dec1 * Inc1
+      
+      densitiesOut <- densitiesOut/sum(densitiesOut)
+      out[[1]] = list(  timeGrid = currentTimeGridOut, densities = densitiesOut, timeScale = timeScale)
+      names(out) <- "Combinaison"
+      class(out) <- "RenDate"
+      return(out)
+  }
+)
 
 # en utilisant, la fonction convole de R
 # les indices imin et imax sont relatifs, c.à.d qu'ils peuvent être négatifs ou nulles
